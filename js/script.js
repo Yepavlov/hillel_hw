dd4d98cc8223a5e9d2a8eb2ba0c2e50896b5470e
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-11-23 17:28:48 -0800

    Homework number 6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,68 @@
 "use strict";
+
+
+function convertToNumber(value) {
+    if (value === null || value.trim() === "") {
+        return null;
+    }
+    const numberValue = Number(value);
+    if (!isNaN(numberValue)) {
+        return numberValue;
+    }
+    return null;
+}
+
+function validateString(name) {
+    if (name === null || name.trim() === "") {
+        return null;
+    }
+    return name.trim();
+}
+
+function convertStringToLikesOrDislikes(value) {
+    if (typeof value !== "string" || value.trim() === "") {
+        return null;
+    }
+    const validatedValue = value.trim().toLowerCase();
+    const allowedValues = ["yes", "no"];
+    if (!allowedValues.includes(validatedValue)) {
+        return null;
+    }
+    return validatedValue === "yes" ? "likes" : "dislikes";
+}
+
+function showMessage(userName, userAge, userCity, loveToJS) {
+    let invalidFields = [];
+    if (!userName) {
+        invalidFields.push("Name")
+    }
+    if (userAge === null) {
+        invalidFields.push("Age")
+    }
+    if (!userCity) {
+        invalidFields.push("City")
+    }
+    if (!loveToJS) {
+        invalidFields.push("Answer about JavaScript (Yes/No)")
+    }
+    if (invalidFields.length > 0) {
+        alert(`Please fix the following fields: ${invalidFields.join(", ")}`);
+        return;
+    }
+    alert(`Hello ${userName}! You are ${userAge} age old, you are from ${userCity} city. Attitude toward JavaScript: ${loveToJS} it.`);
+}
+
+
+const userNameInitial = prompt("Enter your name: ");
+const userName = validateString(userNameInitial);
+
+const userAgeInitial = prompt("Enter your age: ");
+const userAge = convertToNumber(userAgeInitial);
+
+const userCityInitial = prompt("Enter the city where you live: ");
+const userCity = validateString(userCityInitial);
+
+const loveToJS = prompt("Do you like Javascript? \n Please, enter only 'Yes' or 'No' ");
+const isLove = convertStringToLikesOrDislikes(loveToJS);
+
+showMessage(userName, userAge, userCity, isLove);
