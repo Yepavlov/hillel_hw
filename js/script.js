6e805a36fbf83375eb36387b8d42a3529dafd591
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-12-02 18:54:51 -0800

    Homework 13

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const arr = [1, 2, 3, 4, 5];
+
+
+let result = 0;
+
+for (const number of arr) {
+    result += number
+}
+console.log(result);
+
+let result2 = 0;
+for (const number of arr) {
+    result2 += (number ** 2);
+}
+console.log(result2);
