67dd0192ddc2f1ed9b405c102baff227d73625a4
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-12-02 16:42:31 -0800

    Homework 11

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+
+let a = 10;
+let b = 3;
+let num = 5;
+
+
+console.log(a === 0 ? "True" : "False");
+
+console.log(a > 0 ? "True" : "False");
+
+console.log(a < 0 ? "True" : "False");
+
+console.log(a >= 0 ? "True" : "False");
+
+console.log(a <= 0 ? "True" : "False");
+
+console.log(a !== 0 ? "True" : "False");
+
+console.log(a === "test" ? "True" : "False");
+
+console.log(a === "1" ? "True" : "False");
+
+console.log(a > 0 && a < 5 ? "True" : "False");
+
+console.log(a === 0 || a === 2 ? a + 7 : a / 10)
+
+console.log(a <= 1 && b >= 3 ? a + b : a - b);
+
+console.log((a > 2 && a < 11) || (b >= 6 && b < 14) ? "True" : "False");
+
+
+let result;
+switch (num) {
+    case 1:
+        result = "winter";
+        break;
+    case 2:
+        result = "spring";
+        break;
+    case 3:
+        result = "summer";
+        break;
+    case 4:
+        result = "fall";
+        break;
+    default:
+        result = "unknown value";
+}
+console.log(result);
