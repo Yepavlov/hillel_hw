67e59a8abce93ae1a49a487b00b596ea76b76783
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-12-05 19:05:09 -0800

    Homework 14

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+function generateKey(length, characters) {
+  let key = "";
+  const maxIndex = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * maxIndex);
+    key += characters[randomIndex];
+  }
+  return key;
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+const keyLength = 18;
+const key = generateKey(keyLength, characters);
+
+console.assert(
+  key.length === keyLength,
+  "Error: The generated key has the wrong length.",
+);
+
+console.log(`Generated key: ${key}`);
+console.log(`Key Length: ${keyLength}`);
