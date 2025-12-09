73d25d0ac23bbf53ffba836cb71aca49c9795891
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-12-08 19:44:45 -0800

    Homework 16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function removeElement(arr, item) {
+  const index = arr.indexOf(item);
+
+  if (index !== -1) {
+    arr.splice(index, 1);
+  }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
