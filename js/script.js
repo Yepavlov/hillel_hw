0abef1c6dc1120a10c104ae35c9f1c0d1d8ce244
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2025-12-25 16:21:34 -0800

    Fixed myApply and myBind

--- js/script.js
+++ js/script.js
@@ -1,50 +1,64 @@
 "use strict";
 
 const user = {
-  name: "John",
-  age: 25,
-  address: "Texas",
+    name: "John",
+    age: 25,
+    address: "Texas",
 };
 
 function showName() {
-  console.log(this.name);
+    console.log(this.name);
 }
 
 Function.prototype.myCall = function (context, ...args) {
-  const ctx =
-    context === null || context === undefined ? globalThis : Object(context);
-  const uniqueKey = Symbol("customCall");
-  ctx[uniqueKey] = this;
-  const result = ctx[uniqueKey](...args);
-  delete ctx[uniqueKey];
-  return result;
+    const ctx =
+        context === null || context === undefined ? globalThis : Object(context);
+    const uniqueKey = Symbol("customCall");
+    ctx[uniqueKey] = this;
+    const result = ctx[uniqueKey](...args);
+    delete ctx[uniqueKey];
+    return result;
 };
 
 showName.myCall(user);
 
-Function.prototype.myApply = function (context, args = []) {
-  const ctx =
-    context === null || context === undefined ? globalThis : Object(context);
-  const uniqueKey = Symbol("customApply");
-  ctx[uniqueKey] = this;
-  if (!Array.isArray(args) && !args.length) {
-    throw new TypeError("CreateListFromArrayLike called on non-object");
-  }
-  const result = ctx[uniqueKey](...args);
-  delete ctx[uniqueKey];
-  return result;
+Function.prototype.myApply = function (context, args) {
+    const ctx =
+        context === null || context === undefined ? globalThis : Object(context);
+    const uniqueKey = Symbol("customApply");
+    ctx[uniqueKey] = this;
+    let safeArgs = args;
+    if (safeArgs === null || safeArgs === undefined) {
+        safeArgs = [];
+    }
+    if (typeof safeArgs !== 'object') {
+        throw new TypeError("CreateListFromArrayLike called on non-object");
+    }
+    const argsArray = Array.from(safeArgs);
+    const result = ctx[uniqueKey](...argsArray);
+    delete ctx[uniqueKey];
+    return result;
 };
 
 showName.myApply(user);
 
 Function.prototype.myBind = function (context, ...args) {
-  return (...callArgs) => {
-    return this.myCall(context, ...args, ...callArgs);
-  };
+    const originalFn = this;
+    const boundFn = function (...callArgs) {
+        const isConstructor = this instanceof boundFn;
+        const contextToUse = isConstructor ? this : context;
+        return originalFn.myApply(contextToUse, [...args, ...callArgs]);
+    };
+
+    if (originalFn.prototype) {
+        boundFn.prototype = Object.create(originalFn.prototype);
+    }
+
+    return boundFn;
 };
 
 const newShowName = showName.myBind(user);
 
 for (let i = 0; i < 5; i++) {
-  newShowName();
+    newShowName();
 }
