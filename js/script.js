8c840d5457efd613b5df91fd4b495c876770ee91
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2026-01-08 15:04:15 -0800

    Homework 24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const container = document.querySelector("#cards-container");
+
+container.addEventListener("click", function (event) {
+  console.log(event);
+  const btn = event.target.closest("[data-read-more-btn]");
+  if (!btn) return;
+
+  const currentCard = btn.closest(".card");
+
+  const longText = currentCard.querySelector(".long-text");
+  longText.classList.toggle("expanded");
+  btn.textContent = longText.classList.contains("expanded")
+    ? "Hide"
+    : "Read more";
+});
