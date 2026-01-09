467bd3ba226bdd2a2652ad24a966e2e0563bb180
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2026-01-08 16:14:18 -0800

    Homework 25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const input = document.querySelector("#itemInput");
+const addBtn = document.querySelector("#addBtn");
+const list = document.querySelector("#myList");
+
+function addNewTask() {
+  const text = input.value;
+  if (text.trim() === "") return;
+
+  const newLi = document.createElement("li");
+  newLi.append(text);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.append("✖");
+  deleteBtn.classList.add("delete-btn");
+
+  newLi.append(deleteBtn);
+  list.appendChild(newLi);
+  input.value = "";
+}
+
+addBtn.addEventListener("click", () => {
+  addNewTask();
+});
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") addNewTask();
+});
+
+list.addEventListener("click", (event) => {
+  const targetElement = event.target;
+
+  if (targetElement.closest(".delete-btn")) {
+    const itemToDelete = targetElement.closest("li");
+    itemToDelete.remove();
+    return;
+  }
+  const liItem = targetElement.closest("li");
+  if (liItem) liItem.classList.toggle("selected");
+});
