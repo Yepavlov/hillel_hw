ee9ca364bc161c04411840531ad258c9946f973b
YePavlovRobomate <Yevhenii.Pavlov@robomate.tech>
2026-03-08 13:20:42 -0700

    Homework 38

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,16 @@
-import Card from "./components/Card.jsx";
-import React from "react";
+import Card from './components/Card.jsx';
+import React from 'react';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="MyApp d-flex justify-content-center align-items-center vh-100 bg-dark text-light">
-                <Card/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="MyApp d-flex justify-content-center align-items-center vh-100 bg-dark text-light gap-3">
+        <Card title="Only title (without text)" />
+        <Card text="Only text (without title)" />
+        <Card title="Some title" text="Some text" />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-class Card extends React.Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <h4 className="card-title">Card title</h4>
-          <p className="card-text">
-            Some quick example text to build on the card
-          </p>
-          <button type="button" className="btn btn-primary">
-            Go somewhere
-          </button>
-        </div>
+const Card = ({ title, text }) => {
+  return (
+    <div className="card text-dark" style={{ width: '18rem' }}>
+      <div className="card-body">
+        {title && <h4 className="card-title">{title}</h4>}
+        {text && <p className={'card-text'}>{text}</p>}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
